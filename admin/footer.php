10aa9046418167667ff5af1951498ee05336d163
Theekshan Chathuranga <theekshana_20211068@fot.ruh.ac.lk>
2024-10-31 19:23:38 +0530

    Admin part add
    Admin control part add

---
+++ admin/footer.php
@@ -0,0 +1,6 @@
+<script src="https://cdn.amcharts.com/lib/5/index.js"></script>
+<script src="https://cdn.amcharts.com/lib/5/xy.js"></script>
+<script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
+<script src="https://cdn.amcharts.com/lib/5/locales/de_DE.js"></script>
+<script src="https://cdn.amcharts.com/lib/5/geodata/germanyLow.js"></script>
+<script src="https://cdn.amcharts.com/lib/5/fonts/notosans-sc.js"></script>
