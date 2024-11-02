c20318c4defb2996b8acaaa601031e07602c248e
Theekshan Chathuranga <109914468+TheekshanaChathuranga@users.noreply.github.com>
2024-11-02 20:21:48 +0530

    Make some changes

--- index.php
+++ index.php
@@ -63,7 +63,7 @@ if (isset($_POST['signin']))
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Log in</title>
+  <title>Admin Log in</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -77,7 +77,7 @@ if (isset($_POST['signin']))
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="index.php"><b>Admin</b>LTE</a>
+    <a href="index.php"><b>Admin</a>
   </div>
   <!-- /.login-logo -->
   <div class="card">
